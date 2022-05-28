31252d44f95a95f60f3ab70f4be4f6142c718e17
许大仙 <1900919313@qq.com>
2022-05-28 15:21:26 +0800

    [新增功能](master): 交换机的回调接口

--- rabbitmq-springboot/src/main/java/com/github/callback/ConfirmCallback.java
+++ rabbitmq-springboot/src/main/java/com/github/callback/ConfirmCallback.java
@@ -1,10 +1,12 @@
 package com.github.callback;
 
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Optional;
 
 /**
@@ -13,15 +15,26 @@ import java.util.Optional;
  * @since 2022-05-27 13:51:05
  */
 @Slf4j
+@Value
 @Component
 public class ConfirmCallback implements RabbitTemplate.ConfirmCallback {
+
+    RabbitTemplate rabbitTemplate;
+
+    /**
+     * 需要将当前类的对象注入到 RabbitTemplate 中，因为 RabbitTemplate.ConfirmCallback 是一个内部接口
+     */
+    @PostConstruct
+    public void init() {
+        rabbitTemplate.setConfirmCallback(this);
+    }
 
     /**
      * 不管交换机是否收到消息都会触发这个回调方法
      *
      * @param correlationData 回调的相关数据
-     * @param ack             ack
-     * @param cause           回调的原因
+     * @param ack             true 或 false
+     * @param cause           失败的原因
      */
     @Override
     public void confirm(CorrelationData correlationData, boolean ack, String cause) {
